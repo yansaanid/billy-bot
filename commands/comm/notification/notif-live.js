b558952ead7c2b340d87d7bcdbfd4e27717ef712
yansaan <ian.ian.wold76@gmail.com>
2021-03-07 03:58:00 +0700

    modified notification

--- commands/comm/notification/notif-live.js
+++ commands/comm/notification/notif-live.js
@@ -1,13 +1,21 @@
 const live = require("@config/live.json")
+const {
+  botId
+} = require("@config/main.json")
 const getLive = live.live.map(l => l.channel)
 
 module.exports = {
   auto: true,
   channels: getLive,
   userIds: `721964514018590802`,
-  callback: msg => {
+  callback: async msg => {
     for (const lives of live.live) {
       if (msg.channel.id === lives.channel) {
+        await msg.channel.messages.fetch().then(m => {
+          const msgDel = m.filter(ms => ms.author.id == botId)
+          msg.channel.bulkDelete(msgDel)
+        })
+
         msg.channel.send(`Notifikasi ini ditujukan untuk role <@&${lives.role}>, anda bisa mendapatkan update terbaru dari channel ini di <#${live.notifChn}>`)
       }
     }
