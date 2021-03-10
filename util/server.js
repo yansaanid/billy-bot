c022b8e4a96aca21d2e88bdbb2117f1f07c0c224
yansaan <ian.ian.wold76@gmail.com>
2021-03-10 10:11:36 +0700

    fix headler

--- util/server.js
+++ util/server.js
@@ -4,13 +4,14 @@ const server = express()
 const result = require('@util/get-status')
 
 server.all('/', (req, res)=> {
-  res.status(200).json(result())
   res.setHeader('Content-Type', 'text/html')
 
   res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>')
   
   res.end()
 })
+
+server.get('/status', (req, res) => res.json(result()))
 
 function keepAlive() {
   server.listen(3000, ()=> {
