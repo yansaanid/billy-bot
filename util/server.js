9d93790be1294562ec2c01b73fe3290b3fbc6f0b
yansaan <ian.ian.wold76@gmail.com>
2021-03-10 15:52:25 +0700

    add acao

--- util/server.js
+++ util/server.js
@@ -3,10 +3,9 @@ const server = express()
 
 const result = require('@util/get-status')
 
-server.get('/', (req, res)=> {
+server.all('/', (req, res)=> {
   res.setHeader('Content-Type', 'application/json')
-
-  //res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   
   res.status(200).json(result())
   res.end()
