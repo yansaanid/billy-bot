a39e420799a8ab9729c8841a7fa7a48bf55ada4b
yansaan <ian.ian.wold76@gmail.com>
2021-03-14 05:19:21 +0700

    role back

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ require('dotenv/config')
 require('module-alias/register')
 // const mongo = require("@func/util/mongo.js")
 
-const { prefix, botId } = require("@config/main.json")
+const { prefix } = require("@config/main.json")
 const Discord = require("discord.js")
 const chalk = require('chalk')
 const client = new Discord.Client()
 
 const Server = require('@util/server')
-
-const Main = require('@app/Main')
-const main = new Main(prefix,client, botId)
+const loadComm = require('@comm/load-command')
+const loadFunc = require('@feature/load-feature')
 
 const dm = require("@util/dm")
 const loadDm = require("@data/dm.json")
@@ -18,7 +17,8 @@ const loadDm = require("@data/dm.json")
 client.on('ready', async () => {
   console.log(chalk`${chalk.bgYellow.black.bold(client.user.username)} is {bgGreen.white.bold Active}`)
   
-  main.run()
+  loadComm(client)
+  loadFunc(client)
 })
 
 loadDm.forEach(DM => dm(client, DM.send, DM.reply))
